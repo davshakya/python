b194f5d61c512c8a17459c8c75173f0062c4c74a
Devendra Singh Shakya <68561473+davshakya@users.noreply.github.com>
2022-03-18 12:46:31 +0530

    hi

---
+++ Oops Python/Opps_practice.py
@@ -0,0 +1,57 @@
+# class Add:
+#     def addition():
+        
+#         a=3+5
+#         return a
+# x=Add.addition()
+# print(x)    
+
+    
+
+
+
+        
+
+
+
+# class multiply:
+#     def __init__(self,i,j):
+#         self.j=j
+#         self.i=i
+#         k=i*j
+#         print(k)
+# b=multiply(4,5)
+
+
+
+
+# i=lambda x,y:x+y
+# j=i(3,4)    
+# print(j)
+
+# from glob import glob
+
+# i=0
+# def myfun():
+#     global i
+#     i+=1
+#     print("Counting...",i)
+#     myfun()
+# myfun()
+
+# import sys
+# print(sys.getrecursionlimit())
+
+
+# def add():
+#     x=4+6
+#     return x
+# x=add()
+# print(x)
+
+def addd():
+    x=4+6
+    print(x)
+addd()
+
+
---
+++ Oops Python/practice_python.py
@@ -0,0 +1,30 @@
+# class Add:
+#     a=6
+#     def addition(self): 
+#         self.a=self.a+5
+#         print(self.a)        
+# ad=Add()
+# ad.addition()
+
+
+# outVar = 'outside_class'    
+# class Geek:
+#     # print("Outside_class2", outVar)
+  
+#     def access_method(self):
+#         print("Outside_class3", outVar)
+
+# g=Geek()
+# g.access_method()
+
+
+# def f():
+#     global s
+#     s = "Look for Python Section"
+#     print(s) 
+# f()
+# s = "Python is great!"
+# print(s)
+
+a=lambda x:x*x
+print(a(4))
